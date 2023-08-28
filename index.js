59a82f3d7312cb4be8fa98595260042b2b6caa3c
Adriyan-Ahmed-Jubayer <adriyan.khan.jubayer@gmail.com>
2023-08-28 18:02:23 +0600

    Phones Card Section Added Dynamically By JavaScript

--- index.js
+++ index.js
@@ -3,5 +3,30 @@ const LoadData = async () =>{
     const Response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     const Data = await Response.json();
     const Phones = Data.data;
-    console.log(Phones);
+    DisplayPhones(Phones);
 }
+
+
+const DisplayPhones = Phones =>{
+    const PhoneCardContainer = document.getElementById('phones-card-container');
+    Phones.forEach(phone => {
+        console.log(phone);
+        const PhoneCard = document.createElement('div');
+        PhoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        PhoneCard.innerHTML = `
+        <figure class="px-10 pt-10">
+              <img src="${phone.image}" alt="Phone" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+              <h2 class="card-title">${phone.phone_name}</h2>
+              <p>There are many variations of passages of available, but the majority have suffered</p>
+              <div class="card-actions">
+                <button class="btn btn-primary">Show Details</button>
+              </div>
+            </div>
+        `;
+
+        PhoneCardContainer.appendChild(PhoneCard);
+    });
+}
+LoadData()
